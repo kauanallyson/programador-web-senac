6bf0d5603c52e0d167e1752fb7e89f7207f637c5
Kauan Allyson <163926307+kauanallyson@users.noreply.github.com>
2026-02-05 16:59:46 -0300

    adiciona exercicio de manipulacao de texto com jquery

---
+++ jquery/js/max.js
@@ -0,0 +1,47 @@
+$(() => {
+  const cores = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+  ];
+  let indiceCor = 0;
+  let intervalo = null;
+
+  const tututuru = new Audio("./audio/tututuru.mp3");
+
+  const $result = $("#result");
+
+  $("#iniciar").on("click", () => {
+    $result.text("ARCO-ÍRIS!");
+
+    if (!intervalo) {
+      intervalo = setInterval(() => {
+        $result.css("color", cores[indiceCor]);
+        indiceCor = (indiceCor + 1) % cores.length;
+      }, 300);
+    }
+  });
+
+  $("#parar").on("click", () => {
+    clearInterval(intervalo);
+    intervalo = null;
+    $result.text("...").css("color", "black");
+  });
+
+  $("#max").on("mouseover", () => {
+    $("#max").attr("src", "img/max-2.webp");
+    $(".max-wrapper").attr("data-label", "Tutututu");
+    tututuru.play();
+  });
+
+  $("#max").on("mouseout", () => {
+    $("#max").attr("src", "img/max-1.webp");
+    $(".max-wrapper").attr("data-label", "Max Verstappen");
+    tututuru.pause();
+    tututuru.currentTime = 0;
+  });
+});
